3f80a37bc3cd74600235054b5cd80c056cbc8ffe
Amos Wenger <amoswenger@gmail.com>
2022-10-16 00:24:19 +0200

    Headers newtype

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,13 +2,31 @@ use smallvec::SmallVec;
 
 use crate::{bufpool::aggregate::AggregateSlice, parse::h1::Header};
 
-// TODO: this could/should be a multimap. http's multimap is neat but doesn't
-// support `AggregateSlice`. The `HeaderName` type should probably have three
-// variants:
-//   WellKnown (TransferEncoding, Connection, etc.)
-//   &'static [u8] (custom)
-//   AggregateSlice (proxied)
-pub type Headers = SmallVec<[Header; 32]>;
+#[derive(Default)]
+pub struct Headers {
+    // TODO: this could/should be a multimap. http's multimap is neat but doesn't
+    // support `AggregateSlice`. The `HeaderName` type should probably have three
+    // variants:
+    //   WellKnown (TransferEncoding, Connection, etc.)
+    //   &'static [u8] (custom)
+    //   AggregateSlice (proxied)
+    headers: SmallVec<[Header; 32]>,
+}
+
+impl Headers {
+    pub fn push(&mut self, header: Header) {
+        self.headers.push(header);
+    }
+}
+
+impl<'a> IntoIterator for &'a Headers {
+    type Item = &'a Header;
+    type IntoIter = std::slice::Iter<'a, Header>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.headers.iter()
+    }
+}
 
 /// An HTTP request
 pub struct Request {
