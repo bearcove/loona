2f8724e58a85d98cb2149bddf79c4b401602adf6
Amos Wenger <amoswenger@gmail.com>
2022-10-21 18:45:40 +0200

    Introduce some test I/O types

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,6 @@
+use std::{cell::RefCell, rc::Rc};
+
+use tokio::sync::broadcast;
 use tokio_uring::{
     buf::{IoBuf, IoBufMut},
     net::TcpStream,
@@ -45,5 +48,92 @@ impl WriteOwned for TcpStream {
 
     async fn writev<B: IoBuf>(&self, list: Vec<B>) -> BufResult<usize, Vec<B>> {
         TcpStream::writev(self, list).await
+    }
+}
+
+pub struct ChanReadOwned {
+    tx: broadcast::Sender<()>,
+    buffered: Rc<RefCell<Buffered>>,
+}
+
+pub struct ChanReadOwnedSender {
+    tx: broadcast::Sender<()>,
+    buffered: Rc<RefCell<Buffered>>,
+}
+
+#[derive(Default)]
+struct Buffered {
+    pos: usize,
+    buf: Vec<u8>,
+}
+
+impl Buffered {
+    fn is_empty(&self) -> bool {
+        self.pos == self.buf.len()
+    }
+}
+
+impl ChanReadOwned {
+    pub fn new() -> (Self, ChanReadOwnedSender) {
+        let (tx, _) = broadcast::channel(1);
+        let buffered = Rc::new(RefCell::new(Default::default()));
+        (
+            Self {
+                tx: tx.clone(),
+                buffered: buffered.clone(),
+            },
+            ChanReadOwnedSender { tx, buffered },
+        )
+    }
+}
+
+impl ChanReadOwnedSender {
+    pub async fn send(&mut self, buf: Vec<u8>) {
+        loop {
+            {
+                let mut buffered = self.buffered.borrow_mut();
+                if buffered.is_empty() {
+                    buffered.pos = 0;
+                    buffered.buf = buf;
+                    _ = self.tx.send(());
+                    return;
+                }
+            }
+
+            let mut rx = self.tx.subscribe();
+            if rx.recv().await.is_ok() {
+                continue;
+            }
+        }
+    }
+}
+
+impl ReadOwned for ChanReadOwned {
+    async fn read<B: IoBufMut>(&self, mut buf: B) -> BufResult<usize, B> {
+        let out =
+            unsafe { std::slice::from_raw_parts_mut(buf.stable_mut_ptr(), buf.bytes_total()) };
+
+        loop {
+            {
+                let mut current = self.buffered.borrow_mut();
+                let current = &mut current;
+                let remain = current.buf.len() - current.pos;
+
+                if remain > 0 {
+                    let n = std::cmp::min(remain, out.len());
+                    out[..n].copy_from_slice(&current.buf[current.pos..current.pos + n]);
+                    current.pos += n;
+
+                    _ = self.tx.send(());
+                    return (Ok(n), buf);
+                }
+            }
+
+            let mut rx = self.tx.subscribe();
+            match rx.recv().await {
+                Ok(_) => continue,
+                Err(_) => return (Ok(0), buf),
+            }
+        }
     }
 }
