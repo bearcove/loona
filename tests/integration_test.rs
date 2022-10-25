b8c7fa584f53ad47be6db8e75c3bfcb28273bf42
Amos Wenger <amos@bearcove.net>
2022-10-25 15:44:53 +0000

    hook up hyper & hring together

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,7 +5,7 @@
 mod helpers;
 
 use bytes::BytesMut;
-use futures::TryStreamExt;
+use futures::{FutureExt, TryStreamExt};
 use hring::{
     bufpool::AggBuf,
     io::{ChanRead, ChanWrite, ReadWritePair, WriteOwned},
@@ -15,16 +15,17 @@ use hring::{
     types::{Headers, Request, Response},
 };
 use httparse::{Status, EMPTY_HEADER};
+use hyper::service::make_service_fn;
 use pretty_assertions::assert_eq;
 use pretty_hex::PrettyHex;
-use std::{net::SocketAddr, rc::Rc, time::Duration};
+use std::{convert::Infallible, net::SocketAddr, rc::Rc, time::Duration};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
 use tracing::debug;
 
-use crate::helpers::tcp_serve_h1_once;
+use crate::helpers::{sample_hyper_server::TestService, tcp_serve_h1_once};
 
 // Test ideas:
 // headers too large (ups/dos)
@@ -463,14 +464,69 @@ fn request_api() {
 
 #[test]
 fn proxy_verbose() {
+    async fn client(ln_addr: SocketAddr) -> eyre::Result<()> {
+        let mut socket = TcpStream::connect(ln_addr).await?;
+        socket.set_nodelay(true)?;
+
+        let mut buf = BytesMut::with_capacity(256);
+
+        for status in [200, 400, 500] {
+            debug!("Asking for a {status}");
+            socket
+                .write_all(format!("GET /status/{status} HTTP/1.1\r\n\r\n").as_bytes())
+                .await?;
+
+            socket.flush().await?;
+
+            debug!("Reading response...");
+            'read_response: loop {
+                buf.reserve(256);
+                socket.read_buf(&mut buf).await?;
+                debug!("After read, got {} bytes", buf.len());
+
+                let mut headers = [EMPTY_HEADER; 16];
+                let mut res = httparse::Response::new(&mut headers[..]);
+                let _body_offset = match res.parse(&buf[..])? {
+                    Status::Complete(off) => off,
+                    Status::Partial => continue 'read_response,
+                };
+                debug!("Got a complete response");
+                assert_eq!(res.code, Some(status));
+
+                _ = buf.split();
+                break 'read_response;
+            }
+        }
+
+        debug!("Done with client altogether");
+        Ok(())
+    }
+
     helpers::run(async move {
-        let (server_addr, server_fut) = tcp_serve_h1_once()?;
+        let upstream =
+            hyper::Server::bind(&"[::]:0".parse()?).serve(make_service_fn(|_addr| async move {
+                Ok::<_, Infallible>(TestService)
+            }));
+        let upstream_addr = upstream.local_addr();
+        let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+        let upstream = upstream.with_graceful_shutdown(rx.map(|_| ()));
+
+        let upstream_fut = async move {
+            debug!("Started h1 server");
+            upstream.await?;
+            debug!("Done with h1 server");
+            Ok::<_, eyre::Report>(())
+        };
+
+        let ln = tokio_uring::net::TcpListener::bind("[::]:0".parse()?)?;
+        let ln_addr = ln.local_addr()?;
+
         let proxy_fut = async move {
             let conf = ServerConf::default();
             let conf = Rc::new(conf);
 
             struct SDriver {
-                server_addr: SocketAddr,
+                upstream_addr: SocketAddr,
             }
 
             impl h1::ServerDriver for SDriver {
@@ -484,7 +540,8 @@ fn proxy_verbose() {
                     T: WriteOwned + 'static,
                     B: Body + 'static,
                 {
-                    let transport = tokio_uring::net::TcpStream::connect(self.server_addr).await?;
+                    let transport =
+                        tokio_uring::net::TcpStream::connect(self.upstream_addr).await?;
                     let driver = CDriver { res };
 
                     let (req_body, res) = h1::request(transport, req, req_body, driver).await?;
@@ -503,7 +560,7 @@ fn proxy_verbose() {
             where
                 T: WriteOwned,
             {
-                type Return = (H1Responder<T, ResponseDone>);
+                type Return = H1Responder<T, ResponseDone>;
 
                 async fn on_informational_response(&self, res: Response) -> eyre::Result<()> {
                     todo!()
@@ -520,7 +577,21 @@ fn proxy_verbose() {
                     todo!()
                 }
             }
+
+            let (transport, remote_addr) = ln.accept().await?;
+            debug!("Accepted connection from {remote_addr}");
+            let client_buf = AggBuf::default();
+            let driver = SDriver { upstream_addr };
+            h1::serve(transport, conf, client_buf, driver).await?;
+            debug!("Done serving h1 connection, initiating graceful shutdown");
+            drop(tx);
+
+            Ok(())
         };
+
+        let client_fut = client(ln_addr);
+
+        tokio::try_join!(upstream_fut, proxy_fut, client_fut)?;
 
         Ok(())
     });
