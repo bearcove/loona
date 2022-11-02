38fa4d3a1f8799fcba5eb8764f20ea28c3ec39ff
Amos Wenger <amoswenger@gmail.com>
2022-11-02 11:41:24 +0100

    Upgrade tokio-uring

--- tests/helpers/mod.rs
+++ tests/helpers/mod.rs
@@ -9,7 +9,5 @@ pub(crate) fn run(test: impl Future<Output = eyre::Result<()>>) {
         if let Err(e) = test.await {
             panic!("Error: {e:?}");
         }
-
-        std::process::exit(0);
     });
 }
