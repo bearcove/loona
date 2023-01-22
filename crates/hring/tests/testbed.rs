545cc37b041069d8f6898fab6d2c0679e185e6ec
Amos Wenger <amoswenger@gmail.com>
2023-01-22 13:49:20 +0100

    Move hring crate to crates/

--- crates/hring/tests/testbed.rs
+++ crates/hring/tests/testbed.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, net::SocketAddr, process::Stdio};
+use std::{any::Any, net::SocketAddr, path::PathBuf, process::Stdio};
 
 use tokio::{
     io::{AsyncBufReadExt, BufReader},
@@ -10,9 +10,16 @@ pub async fn start() -> eyre::Result<(SocketAddr, impl Any)> {
     let (addr_tx, addr_rx) = tokio::sync::oneshot::channel::<SocketAddr>();
 
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let mut cmd = Command::new(format!(
-        "{manifest_dir}/test-crates/hyper-testbed/target/release/hyper-testbed"
-    ));
+    let manifest_dir = PathBuf::from(manifest_dir);
+    let project_dir = manifest_dir.parent().unwrap().parent().unwrap();
+    let test_crates_dir = project_dir.join("test-crates");
+    let hyper_testbed_dir = test_crates_dir.join("hyper-testbed");
+    let binary_path = hyper_testbed_dir
+        .join("target")
+        .join("release")
+        .join("hyper-testbed");
+    debug!("Using testbed binary: {}", binary_path.display());
+    let mut cmd = Command::new(binary_path);
     cmd.stdout(Stdio::piped());
     cmd.kill_on_drop(true);
 
