d831b0d31b4c59bd7d578c0aa6ca8427b035a694
Amos Wenger <amoswenger@gmail.com>
2023-02-20 17:27:36 +0100

    Introduce ConnSettings

--- crates/hring/src/h2/server.rs
+++ crates/hring/src/h2/server.rs
@@ -43,6 +43,96 @@ impl Default for ServerConf {
 #[derive(Default)]
 struct ConnState {
     streams: HashMap<StreamId, StreamState>,
+    our_settings: ConnSettings,
+    peer_settings: ConnSettings,
+}
+
+/// cf. https://httpwg.org/specs/rfc9113.html#SettingValues
+#[derive(Clone, Copy)]
+struct ConnSettings {
+    /// This setting allows the sender to inform the remote endpoint of the
+    /// maximum size of the compression table used to decode field blocks, in
+    /// units of octets. The encoder can select any size equal to or less than
+    /// this value by using signaling specific to the compression format inside
+    /// a field block (see [COMPRESSION]). The initial value is 4,096 octets.
+    header_table_size: u32,
+
+    /// This setting can be used to enable or disable server push. A server MUST
+    /// NOT send a PUSH_PROMISE frame if it receives this parameter set to a
+    /// value of 0; see Section 8.4. A client that has both set this parameter
+    /// to 0 and had it acknowledged MUST treat the receipt of a PUSH_PROMISE
+    /// frame as a connection error (Section 5.4.1) of type PROTOCOL_ERROR.
+    ///
+    /// The initial value of SETTINGS_ENABLE_PUSH is 1. For a client, this value
+    /// indicates that it is willing to receive PUSH_PROMISE frames. For a
+    /// server, this initial value has no effect, and is equivalent to the value
+    /// 0. Any value other than 0 or 1 MUST be treated as a connection error
+    /// (Section 5.4.1) of type PROTOCOL_ERROR.
+    ///
+    /// A server MUST NOT explicitly set this value to 1. A server MAY choose to
+    /// omit this setting when it sends a SETTINGS frame, but if a server does
+    /// include a value, it MUST be 0. A client MUST treat receipt of a SETTINGS
+    /// frame with SETTINGS_ENABLE_PUSH set to 1 as a connection error (Section
+    /// 5.4.1) of type PROTOCOL_ERROR.
+    enable_push: bool,
+
+    /// This setting indicates the maximum number of concurrent streams that the
+    /// sender will allow. This limit is directional: it applies to the number of
+    /// streams that the sender permits the receiver to create. Initially, there is
+    /// no limit to this value. It is recommended that this value be no smaller than
+    /// 100, so as to not unnecessarily limit parallelism.
+    ///
+    /// A value of 0 for SETTINGS_MAX_CONCURRENT_STREAMS SHOULD NOT be treated as
+    /// special by endpoints. A zero value does prevent the creation of new streams;
+    /// however, this can also happen for any limit that is exhausted with active
+    /// streams. Servers SHOULD only set a zero value for short durations; if a
+    /// server does not wish to accept requests, closing the connection is more
+    /// appropriate.
+    max_concurrent_streams: u32,
+
+    /// This setting indicates the sender's initial window size (in units of
+    /// octets) for stream-level flow control. The initial value is 2^16-1
+    /// (65,535) octets.
+    ///
+    /// This setting affects the window size of all streams (see Section 6.9.2).
+    ///
+    /// Values above the maximum flow-control window size of 23^1-1 MUST be
+    /// treated as a connection error (Section 5.4.1) of type
+    /// FLOW_CONTROL_ERROR.
+    initial_window_size: u32,
+
+    /// This setting indicates the size of the largest frame payload that the
+    /// sender is willing to receive, in units of octets.
+    ///
+    /// The initial value is 2^14 (16,384) octets. The value advertised by an
+    /// endpoint MUST be between this initial value and the maximum allowed frame
+    /// size (224-1 or 16,777,215 octets), inclusive. Values outside this range MUST
+    /// be treated as a connection error (Section 5.4.1) of type PROTOCOL_ERROR.
+    max_frame_size: u32,
+
+    /// This advisory setting informs a peer of the maximum field section size
+    /// that the sender is prepared to accept, in units of octets. The value is
+    /// based on the uncompressed size of field lines, including the length of
+    /// the name and value in units of octets plus an overhead of 32 octets for
+    /// each field line.
+    ///
+    /// For any given request, a lower limit than what is advertised MAY be
+    /// enforced. The initial value of this setting is unlimited.
+    max_header_list_size: u32,
+}
+
+impl Default for ConnSettings {
+    fn default() -> Self {
+        // cf. https://httpwg.org/specs/rfc9113.html#SettingValues
+        Self {
+            header_table_size: 4096,
+            enable_push: false,
+            max_concurrent_streams: 100,
+            initial_window_size: (2 << 16) - 1,
+            max_frame_size: (2 << 14),
+            max_header_list_size: 0,
+        }
+    }
 }
 
 #[derive(Default, Clone, Copy)]
