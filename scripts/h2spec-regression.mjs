efde3fca482abe4eecd57fd8156d911cf7d65b41
Amos Wenger <amos@bearcove.net>
2023-11-04 17:57:45 +0100

    Parse current test results

--- scripts/h2spec-regression.mjs
+++ scripts/h2spec-regression.mjs
@@ -1,4 +1,34 @@
 import { Octokit } from "octokit";
+import { XMLParser } from "fast-xml-parser";
+import fs from "fs/promises";
+
+let xmlParser = new XMLParser({
+  ignoreAttributes: false,
+});
+let http2Result = xmlParser.parse(await fs.readFile("target/h2spec-http2.xml"));
+console.log(JSON.stringify(http2Result, null, 2));
+
+let results = {
+  total: 0,
+  passed: 0,
+  failed: 0,
+  skipped: 0,
+};
+
+for (const ts of http2Result.testsuites.testsuite) {
+  let tests = parseInt(ts["@_tests"], 10);
+  let failures = parseInt(ts["@_failures"], 10);
+  let errors = parseInt(ts["@_errors"], 10);
+  let skipped = parseInt(ts["@_skipped"], 10);
+
+  results.total += tests;
+  results.failed += failures + errors;
+  results.skipped += skipped;
+  results.passed += tests - (failures + errors + skipped);
+}
+console.log(JSON.stringify(results, null, 2));
+
+process.exit(0);
 
 const octokit = new Octokit({
   auth: process.env.TOKEN,
@@ -16,28 +46,23 @@ if (!http2Check) {
   process.exit(0);
 }
 
-console.log("output summary: ", JSON.stringify(http2Check.output.summary));
-let http2CheckResults = extractTestResults(http2Check.output.summary);
+let http2CheckResults = extractTestResultsFromCheckSummary(
+  http2Check.output.summary,
+);
 console.log(JSON.stringify(http2CheckResults));
 
-function extractTestResults(resultString) {
+function extractTestResultsFromCheckSummary(resultString) {
   // example input:
-  // **94** tests were completed in **NaNms** with **70** passed, **24** failed and **0** skipped
+  //   **94** tests were completed in **NaNms** with **70** passed, **24** failed and **0** skipped
   // example output:
-  // {
-  //   total: 94,
-  //   passed: 70,
-  //   failed: 24,
-  //   skipped: 0,
-  // }
-  // using a regexp:
+  //   { total: 94, passed: 70, failed: 24, skipped: 0 }
   const regex =
     /[*][*](\d+)[*][*] tests were completed in [*][*](\w+)ms[*][*] with [*][*](\d+)[*][*] passed, [*][*](\d+)[*][*] failed and [*][*](\d+)[*][*] skipped/g;
   let m = regex.exec(resultString);
   return {
-    total: parseInt(m[1]),
-    passed: parseInt(m[3]),
-    failed: parseInt(m[4]),
-    skipped: parseInt(m[5]),
+    total: parseInt(m[1], 10),
+    passed: parseInt(m[3], 10),
+    failed: parseInt(m[4], 10),
+    skipped: parseInt(m[5], 10),
   };
 }
