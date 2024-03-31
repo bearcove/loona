0acf2f09d9dd6af62144a53ff461f3dee8deff34
Amos Wenger <amos@bearcove.net>
2024-03-31 22:37:14 +0200

    Fix testbed path

--- crates/fluke-curl-tests/tests/helpers/tracing_common.rs
+++ crates/fluke-curl-tests/tests/helpers/tracing_common.rs
@@ -6,10 +6,14 @@ use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::Subscriber
 /// This won't play well outside of cargo-nextest or running a single
 /// test, which is a limitation we accept.
 pub(crate) fn setup_tracing() {
-    let filter_layer = Targets::new()
-        .with_default(Level::INFO)
-        .with_target("fluke", Level::DEBUG)
-        .with_target("want", Level::INFO);
+    let targets = if let Ok(rust_log) = std::env::var("RUST_LOG") {
+        rust_log.parse::<Targets>().unwrap()
+    } else {
+        Targets::new()
+            .with_default(Level::INFO)
+            .with_target("fluke", Level::DEBUG)
+            .with_target("want", Level::INFO)
+    };
 
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_ansi(true)
@@ -17,7 +21,7 @@ pub(crate) fn setup_tracing() {
         .with_line_number(false);
 
     tracing_subscriber::registry()
-        .with(filter_layer)
+        .with(targets)
         .with(fmt_layer)
         .init();
 }
