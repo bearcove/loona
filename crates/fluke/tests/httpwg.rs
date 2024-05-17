23410ee60428ca0c55043b10b42fdce5d98b476a
Amos Wenger <amos@bearcove.net>
2024-05-17 12:47:23 +0200

    Introduce start_server function

--- crates/fluke/tests/httpwg.rs
+++ crates/fluke/tests/httpwg.rs
@@ -1,7 +1,7 @@
 use std::{rc::Rc, sync::Arc};
 
 use fluke::{Body, Encoder, ExpectResponseHeaders, Responder, Response, ResponseDone};
-use fluke_buffet::{IntoHalves, ReadOwned, RollMut, WriteOwned};
+use fluke_buffet::{IntoHalves, PipeRead, PipeWrite, ReadOwned, RollMut, WriteOwned};
 use http::StatusCode;
 use tracing::Level;
 use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::SubscriberInitExt};
@@ -63,6 +63,52 @@ impl fluke::ServerDriver for TestDriver {
     }
 }
 
+pub struct TwoHalves<W, R>(W, R);
+impl<W: WriteOwned, R: ReadOwned> IntoHalves for TwoHalves<W, R> {
+    type Read = R;
+    type Write = W;
+
+    fn into_halves(self) -> (Self::Read, Self::Write) {
+        (self.1, self.0)
+    }
+}
+
+pub fn start_server() -> httpwg::Conn<TwoHalves<PipeWrite, PipeRead>> {
+    let (server_write, client_read) = fluke::buffet::pipe();
+    let (client_write, server_read) = fluke::buffet::pipe();
+
+    let serve_fut = async move {
+        let conf = fluke::h2::ServerConf {
+            ..Default::default()
+        };
+
+        let client_buf = RollMut::alloc()?;
+        let driver = Rc::new(TestDriver);
+        fluke::h2::serve(
+            (server_read, server_write),
+            Rc::new(conf),
+            client_buf,
+            driver,
+        )
+        .await?;
+        tracing::debug!("http/2 server done");
+        Ok::<_, eyre::Report>(())
+    };
+
+    fluke_buffet::spawn(async move {
+        serve_fut.await.unwrap();
+    });
+
+    httpwg::Conn::new(TwoHalves(client_write, client_read))
+}
+
+// httpwg::gen_tests! {
+//     setup_tracing();
+
+//     async fn inner() -> eyre::Result<()> {
+//     }
+// };
+
 #[test]
 fn httpwg() {
     setup_tracing();
@@ -72,46 +118,8 @@ fn httpwg() {
         for group in groups {
             println!("Group: {}", group.name);
             for test in group.tests {
-                tracing::debug!("Starting test...");
-
-                let (server_write, client_read) = fluke::buffet::pipe();
-                let (client_write, server_read) = fluke::buffet::pipe();
-
-                let serve_fut = async move {
-                    let conf = fluke::h2::ServerConf {
-                        ..Default::default()
-                    };
-
-                    let client_buf = RollMut::alloc()?;
-                    let driver = Rc::new(TestDriver);
-                    fluke::h2::serve(
-                        (server_read, server_write),
-                        Rc::new(conf),
-                        client_buf,
-                        driver,
-                    )
-                    .await?;
-                    tracing::debug!("http/2 server done");
-                    Ok::<_, eyre::Report>(())
-                };
-
-                fluke_buffet::spawn(async move {
-                    serve_fut.await.unwrap();
-                });
-
+                let conn = start_server();
                 let config = Arc::new(httpwg::Config {});
-
-                struct TwoHalves<W, R>(W, R);
-                impl<W: WriteOwned, R: ReadOwned> IntoHalves for TwoHalves<W, R> {
-                    type Read = R;
-                    type Write = W;
-
-                    fn into_halves(self) -> (Self::Read, Self::Write) {
-                        (self.1, self.0)
-                    }
-                }
-
-                let conn = httpwg::Conn::new(TwoHalves(client_write, client_read));
                 let result = test.run(config, conn).await;
                 println!("  Test: {:?}", result);
             }
