83f2cab3fa56fc6f494cbe3e5ee0b97a6e029d8c
Amos Wenger <amos@bearcove.net>
2024-08-11 11:43:18 +0200

    yep that's what's happening

--- crates/fluke-io-uring-async/src/linux.rs
+++ crates/fluke-io-uring-async/src/linux.rs
@@ -90,6 +90,23 @@ impl<C: cqueue::Entry> Drop for Op<C> {
                 std::mem::forget(cancel_op);
                 tracing::debug!("doing async cancel for op {index} (cancel_fut forgotten)");
 
+                struct NoisyDrop;
+
+                impl Drop for NoisyDrop {
+                    fn drop(&mut self) {
+                        tracing::debug!("dropping noisy drop");
+                    }
+                }
+
+                let noisy_drop = NoisyDrop;
+                tracing::debug!("spawning noisy drop task");
+                tokio::task::spawn_local(async move {
+                    tracing::debug!("inside noisy drop spawned task");
+                    drop(noisy_drop);
+                    tracing::debug!("inside noisy drop spawned task.. dropped!");
+                });
+                tracing::debug!("spawned noisy drop task");
+
                 tokio::task::spawn_local(async move {
                     tracing::debug!("cancelling op {index}");
                     cancel_op_inner.await;
