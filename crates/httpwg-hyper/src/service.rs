7e15f28093a0f7524ed78efa12b97a37ff431d4c
Amos Wenger <amos@bearcove.net>
2024-08-30 19:09:07 +0200

    Implement standard endpoints for httpwg-loona (save for /stream-file)

--- crates/httpwg-hyper/src/service.rs
+++ crates/httpwg-hyper/src/service.rs
@@ -1,3 +1,13 @@
+//! //! This service provides the following routes:
+//!
+//! - `/echo-body`: Echoes back the request body.
+//! - `/status/{code}`: Returns a response with the specified status code.
+//! - `/repeat-4k-blocks/{repeat}`: Streams the specified number of 4KB blocks.
+//! - `/stream-file/{name}`: Streams the contents of a file from
+//!   `/tmp/stream-file/`.
+//! - `/`: Returns a default message.
+//! - Any other path: Returns a 404 Not Found response.
+
 use http_body_util::{BodyExt, StreamBody};
 use httpwg_harness::SAMPLE_4K_BLOCK;
 use tokio::io::AsyncReadExt;
@@ -34,93 +44,89 @@ where
             let (parts, mut req_body) = req.into_parts();
 
             let path = parts.uri.path();
-            match path {
-                "/echo-body" => {
-                    let body: BoxBody<E> = Box::pin(req_body);
+            let parts = path.trim_start_matches('/').split('/').collect::<Vec<_>>();
+
+            if let ["echo-body"] = parts.as_slice() {
+                let body: BoxBody<E> = Box::pin(req_body);
+                let res = Response::builder().body(body).unwrap();
+                Ok(res)
+            } else {
+                let body: BoxBody<E> =
+                    Box::pin(http_body_util::Empty::new().map_err(|_| unreachable!()));
+
+                if let ["status", code] = parts.as_slice() {
+                    // drain body
+                    while let Some(_frame) = req_body.frame().await {}
+
+                    let code = code.parse::<u16>().unwrap();
+                    let res = Response::builder().status(code).body(body).unwrap();
+                    debug!("Replying with {:?} {:?}", res.status(), res.headers());
+                    Ok(res)
+                } else if let ["repeat-4k-blocks", repeat] = parts.as_slice() {
+                    // drain body
+                    while let Some(_frame) = req_body.frame().await {}
+
+                    let repeat = repeat.parse::<usize>().unwrap();
+
+                    // TODO: custom impl of the Body trait to avoid channel overhead
+                    let (tx, rx) = mpsc::channel::<Result<Frame<Bytes>, E>>(1);
+
+                    tokio::spawn(async move {
+                        let block = Bytes::copy_from_slice(SAMPLE_4K_BLOCK);
+                        for _ in 0..repeat {
+                            let frame = Frame::data(block.clone());
+                            let _ = tx.send(Ok(frame)).await;
+                        }
+                    });
+
+                    let rx = ReceiverStream::new(rx);
+                    let body: BoxBody<E> = Box::pin(StreamBody::new(rx));
                     let res = Response::builder().body(body).unwrap();
                     Ok(res)
-                }
-                _ => {
-                    let parts = path.trim_start_matches('/').split('/').collect::<Vec<_>>();
+                } else if let ["stream-file", name] = parts.as_slice() {
+                    // drain body
+                    while let Some(_frame) = req_body.frame().await {}
 
-                    let body: BoxBody<E> =
-                        Box::pin(http_body_util::Empty::new().map_err(|_| unreachable!()));
+                    let name = name.to_string();
 
-                    if let ["status", code] = parts.as_slice() {
-                        // drain body
-                        while let Some(_frame) = req_body.frame().await {}
-
-                        let code = code.parse::<u16>().unwrap();
-                        let res = Response::builder().status(code).body(body).unwrap();
-                        debug!("Replying with {:?} {:?}", res.status(), res.headers());
-                        Ok(res)
-                    } else if let ["repeat-4k-blocks", repeat] = parts.as_slice() {
-                        // drain body
-                        while let Some(_frame) = req_body.frame().await {}
-
-                        let repeat = repeat.parse::<usize>().unwrap();
-
-                        // TODO: custom impl of the Body trait to avoid channel overhead
-                        let (tx, rx) = mpsc::channel::<Result<Frame<Bytes>, E>>(1);
-
-                        tokio::spawn(async move {
-                            let block = Bytes::copy_from_slice(SAMPLE_4K_BLOCK);
-                            for _ in 0..repeat {
-                                let frame = Frame::data(block.clone());
-                                let _ = tx.send(Ok(frame)).await;
+                    // TODO: custom impl of the Body trait to avoid channel overhead
+                    // stream 64KB blocks of the file
+                    let (tx, rx) = mpsc::channel::<Result<Frame<Bytes>, E>>(1);
+                    tokio::spawn(async move {
+                        let mut file = tokio::fs::File::open(format!("/tmp/stream-file/{name}"))
+                            .await
+                            .unwrap();
+                        let mut buf = vec![0u8; 64 * 1024];
+                        while let Ok(n) = file.read(&mut buf).await {
+                            if n == 0 {
+                                break;
                             }
-                        });
+                            let frame = Frame::data(Bytes::copy_from_slice(&buf[..n]));
+                            let _ = tx.send(Ok(frame)).await;
+                        }
+                    });
 
-                        let rx = ReceiverStream::new(rx);
-                        let body: BoxBody<E> = Box::pin(StreamBody::new(rx));
-                        let res = Response::builder().body(body).unwrap();
-                        Ok(res)
-                    } else if let ["stream-file", name] = parts.as_slice() {
-                        // drain body
-                        while let Some(_frame) = req_body.frame().await {}
+                    let rx = ReceiverStream::new(rx);
+                    let body: BoxBody<E> = Box::pin(StreamBody::new(rx));
+                    let res = Response::builder().body(body).unwrap();
+                    Ok(res)
+                } else if parts.as_slice().is_empty() {
+                    // drain body
+                    while let Some(_frame) = req_body.frame().await {}
 
-                        let name = name.to_string();
+                    let body = "it's less dire to lose, than to lose oneself".to_string();
+                    let body: BoxBody<E> = Box::pin(body.map_err(|_| unreachable!()));
+                    let res = Response::builder().status(200).body(body).unwrap();
+                    Ok(res)
+                } else {
+                    // drain body
+                    while let Some(_frame) = req_body.frame().await {}
 
-                        // TODO: custom impl of the Body trait to avoid channel overhead
-                        // stream 64KB blocks of the file
-                        let (tx, rx) = mpsc::channel::<Result<Frame<Bytes>, E>>(1);
-                        tokio::spawn(async move {
-                            let mut file =
-                                tokio::fs::File::open(format!("/tmp/stream-file/{name}"))
-                                    .await
-                                    .unwrap();
-                            let mut buf = vec![0u8; 64 * 1024];
-                            while let Ok(n) = file.read(&mut buf).await {
-                                if n == 0 {
-                                    break;
-                                }
-                                let frame = Frame::data(Bytes::copy_from_slice(&buf[..n]));
-                                let _ = tx.send(Ok(frame)).await;
-                            }
-                        });
-
-                        let rx = ReceiverStream::new(rx);
-                        let body: BoxBody<E> = Box::pin(StreamBody::new(rx));
-                        let res = Response::builder().body(body).unwrap();
-                        Ok(res)
-                    } else if parts.as_slice().is_empty() {
-                        // drain body
-                        while let Some(_frame) = req_body.frame().await {}
-
-                        let body = "it's less dire to lose, than to lose oneself".to_string();
-                        let body: BoxBody<E> = Box::pin(body.map_err(|_| unreachable!()));
-                        let res = Response::builder().status(200).body(body).unwrap();
-                        Ok(res)
-                    } else {
-                        // drain body
-                        while let Some(_frame) = req_body.frame().await {}
-
-                        // return a 404
-                        let body = "404 Not Found".to_string();
-                        let body: BoxBody<E> = Box::pin(body.map_err(|_| unreachable!()));
-                        let res = Response::builder().status(404).body(body).unwrap();
-                        Ok(res)
-                    }
+                    // return a 404
+                    let body = "404 Not Found".to_string();
+                    let body: BoxBody<E> = Box::pin(body.map_err(|_| unreachable!()));
+                    let res = Response::builder().status(404).body(body).unwrap();
+                    Ok(res)
                 }
             }
         })
--- crates/httpwg-loona/src/driver.rs
+++ crates/httpwg-loona/src/driver.rs
@@ -1,12 +1,10 @@
 use b_x::{BxForResults, BX};
-use std::io::Write;
+use httpwg_harness::SAMPLE_4K_BLOCK;
 
-use buffet::{Piece, RollMut};
+use buffet::Piece;
 use loona::{
-    error::NeverError,
-    http::{self, StatusCode},
-    Body, BodyChunk, Encoder, ExpectResponseHeaders, Responder, Response, ResponseDone,
-    ServerDriver, SinglePieceBody,
+    error::NeverError, http::StatusCode, Body, BodyChunk, Encoder, ExpectResponseHeaders,
+    HeadersExt, Responder, Response, ResponseDone, ServerDriver, SinglePieceBody,
 };
 
 pub(super) struct TestDriver;
@@ -23,19 +21,23 @@ where
         req_body: &mut impl Body,
         mut res: Responder<OurEncoder, ExpectResponseHeaders>,
     ) -> Result<Responder<OurEncoder, ResponseDone>, Self::Error> {
-        // if the client sent `expect: 100-continue`, we must send a 100 status code
-        if let Some(h) = req.headers.get(http::header::EXPECT) {
-            if &h[..] == b"100-continue" {
-                res.write_interim_response(Response {
-                    status: StatusCode::CONTINUE,
-                    ..Default::default()
-                })
-                .await?;
-            }
+        if req.headers.expects_100_continue() {
+            res.write_interim_response(Response {
+                status: StatusCode::CONTINUE,
+                ..Default::default()
+            })
+            .await?;
         }
 
-        let res = match req.uri.path() {
-            "/echo-body" => res
+        let parts = req
+            .uri
+            .path()
+            .trim_start_matches('/')
+            .split('/')
+            .collect::<Vec<_>>();
+
+        let res = match parts.as_slice() {
+            ["echo-body"] => res
                 .write_final_response_with_body(
                     Response {
                         status: StatusCode::OK,
@@ -45,114 +47,126 @@ where
                 )
                 .await
                 .bx()?,
-            "/stream-big-body" => {
-                // then read the full request body
-                let mut req_body_len = 0;
-                loop {
-                    let chunk = req_body.next_chunk().await.bx()?;
-                    match chunk {
-                        BodyChunk::Done { trailers } => {
-                            // yey
-                            if let Some(trailers) = trailers {
-                                tracing::debug!(trailers_len = %trailers.len(), "received trailers");
-                            }
-                            break;
-                        }
-                        BodyChunk::Chunk(chunk) => {
-                            req_body_len += chunk.len();
-                        }
-                    }
-                }
-                tracing::debug!(%req_body_len, "read request body");
+            ["status", code] => {
+                drain_body(req_body).await?;
 
-                let mut roll = RollMut::alloc().bx()?;
-                for _ in 0..256 {
-                    roll.write_all("this is a big chunk".as_bytes()).bx()?;
-                }
+                let code = code.parse::<u16>().bx()?;
+                res.write_final_response_with_body(
+                    Response {
+                        status: StatusCode::from_u16(code).bx()?,
+                        ..Default::default()
+                    },
+                    &mut (),
+                )
+                .await
+                .bx()?
+            }
+            ["repeat-4k-blocks", repeat] => {
+                drain_body(req_body).await?;
 
-                struct RepeatBody {
-                    piece: Piece,
-                    n: usize,
-                    written: usize,
-                }
-
-                impl std::fmt::Debug for RepeatBody {
-                    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                        f.debug_struct("RepeatBody")
-                            .field("piece_len", &self.piece.len())
-                            .field("n", &self.n)
-                            .field("written", &self.written)
-                            .finish()
-                    }
-                }
-
-                impl Body for RepeatBody {
-                    type Error = NeverError;
-
-                    fn content_len(&self) -> Option<u64> {
-                        Some(self.n as u64 * self.piece.len() as u64)
-                    }
-
-                    fn eof(&self) -> bool {
-                        self.written == self.n
-                    }
-
-                    async fn next_chunk(&mut self) -> Result<BodyChunk, Self::Error> {
-                        if self.eof() {
-                            return Ok(BodyChunk::Done { trailers: None });
-                        }
-
-                        let chunk = self.piece.clone();
-                        self.written += 1;
-                        Ok(BodyChunk::Chunk(chunk))
-                    }
-                }
-
+                let repeat = repeat.parse::<usize>().bx()?;
                 res.write_final_response_with_body(
                     Response {
                         status: StatusCode::OK,
                         ..Default::default()
                     },
                     &mut RepeatBody {
-                        piece: roll.take_all().into(),
-                        n: 128,
+                        piece: SAMPLE_4K_BLOCK.into(),
+                        n: repeat,
                         written: 0,
                     },
                 )
                 .await
                 .bx()?
             }
-            _ => {
-                // then read the full request body
-                let mut req_body_len = 0;
-                loop {
-                    let chunk = req_body.next_chunk().await.bx()?;
-                    match chunk {
-                        BodyChunk::Done { trailers } => {
-                            // yey
-                            if let Some(trailers) = trailers {
-                                tracing::debug!(trailers_len = %trailers.len(), "received trailers");
-                            }
-                            break;
-                        }
-                        BodyChunk::Chunk(chunk) => {
-                            req_body_len += chunk.len();
-                        }
-                    }
-                }
-                tracing::debug!(%req_body_len, "read request body");
+            ["stream-file", name] => {
+                drain_body(req_body).await?;
+                let _ = name;
 
                 res.write_final_response_with_body(
                     Response {
-                        status: StatusCode::OK,
+                        status: StatusCode::INTERNAL_SERVER_ERROR,
                         ..Default::default()
                     },
-                    &mut SinglePieceBody::from("it's less dire to lose, than to lose oneself"),
+                    &mut SinglePieceBody::from("/stream-file: not implemented yet"),
+                )
+                .await
+                .bx()?
+            }
+            _ => {
+                drain_body(req_body).await?;
+
+                // return a 404
+                res.write_final_response_with_body(
+                    Response {
+                        status: StatusCode::NOT_FOUND,
+                        ..Default::default()
+                    },
+                    &mut SinglePieceBody::from("404 Not Found"),
                 )
                 .await
                 .bx()?
             }
         };
         Ok(res)
+    }
+}
+
+async fn drain_body(body: &mut impl Body) -> Result<(), BX> {
+    let mut req_body_len = 0;
+    loop {
+        let chunk = body.next_chunk().await.bx()?;
+        match chunk {
+            BodyChunk::Done { trailers } => {
+                // yey
+                if let Some(trailers) = trailers {
+                    tracing::debug!(trailers_len = %trailers.len(), "received trailers");
+                }
+                break;
+            }
+            BodyChunk::Chunk(chunk) => {
+                req_body_len += chunk.len();
+            }
+        }
+    }
+    tracing::debug!(%req_body_len, "read request body");
+    Ok(())
+}
+
+struct RepeatBody {
+    piece: Piece,
+    n: usize,
+    written: usize,
+}
+
+impl std::fmt::Debug for RepeatBody {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("RepeatBody")
+            .field("piece_len", &self.piece.len())
+            .field("n", &self.n)
+            .field("written", &self.written)
+            .finish()
+    }
+}
+
+impl Body for RepeatBody {
+    type Error = NeverError;
+
+    fn content_len(&self) -> Option<u64> {
+        Some(self.n as u64 * self.piece.len() as u64)
+    }
+
+    fn eof(&self) -> bool {
+        self.written == self.n
+    }
+
+    async fn next_chunk(&mut self) -> Result<BodyChunk, Self::Error> {
+        if self.eof() {
+            return Ok(BodyChunk::Done { trailers: None });
+        }
+
+        let chunk = self.piece.clone();
+        self.written += 1;
+        Ok(BodyChunk::Chunk(chunk))
     }
 }
