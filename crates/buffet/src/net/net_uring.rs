495dbbe796565c84c2379b8ec17596ddb3a0b5f3
Amos Wenger <amos@bearcove.net>
2024-08-30 15:51:39 +0200

    comment out writev_owned

--- crates/buffet/src/net/net_uring.rs
+++ crates/buffet/src/net/net_uring.rs
@@ -168,34 +168,34 @@ impl WriteOwned for TcpWriteHalf {
 
     // TODO: implement writev
 
-    async fn writev_owned(&mut self, list: &PieceList) -> std::io::Result<usize> {
-        let mut iovecs: Vec<iovec> = vec![];
-        for piece in list.pieces.iter() {
-            let iov = iovec {
-                iov_base: piece.as_ref().as_ptr() as *mut libc::c_void,
-                iov_len: piece.len(),
-            };
-            iovecs.push(iov);
-        }
-        let iovecs = iovecs.into_boxed_slice();
-        let iov_cnt = iovecs.len();
-        // FIXME: don't leak, duh
-        let iovecs = Box::leak(iovecs);
+    // async fn writev_owned(&mut self, list: &PieceList) -> std::io::Result<usize>
+    // {     let mut iovecs: Vec<iovec> = vec![];
+    //     for piece in list.pieces.iter() {
+    //         let iov = iovec {
+    //             iov_base: piece.as_ref().as_ptr() as *mut libc::c_void,
+    //             iov_len: piece.len(),
+    //         };
+    //         iovecs.push(iov);
+    //     }
+    //     let iovecs = iovecs.into_boxed_slice();
+    //     let iov_cnt = iovecs.len();
+    //     // FIXME: don't leak, duh
+    //     let iovecs = Box::leak(iovecs);
 
-        let sqe = Writev::new(
-            io_uring::types::Fd(self.0.fd),
-            iovecs.as_ptr() as *const _,
-            iov_cnt as u32,
-        )
-        .build();
+    //     let sqe = Writev::new(
+    //         io_uring::types::Fd(self.0.fd),
+    //         iovecs.as_ptr() as *const _,
+    //         iov_cnt as u32,
+    //     )
+    //     .build();
 
-        let cqe = get_ring().push(sqe).await;
-        let ret = match cqe.error_for_errno() {
-            Ok(ret) => ret,
-            Err(e) => return Err(std::io::Error::from(e)),
-        };
-        Ok(ret as usize)
-    }
+    //     let cqe = get_ring().push(sqe).await;
+    //     let ret = match cqe.error_for_errno() {
+    //         Ok(ret) => ret,
+    //         Err(e) => return Err(std::io::Error::from(e)),
+    //     };
+    //     Ok(ret as usize)
+    // }
 
     async fn shutdown(&mut self) -> std::io::Result<()> {
         tracing::debug!("requesting shutdown");
